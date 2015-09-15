dceb54a79b27799c857bb9adc2f45081cb1505c3
Zev Benjamin <zev@mit.edu>
2015-09-14 17:34:41 -0700

    Add emacs local machine config

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,2 +1,7 @@
 (load "~/.emacs.d/load-directory")
 (load-directory "~/.emacs.d/config")
+
+;;; Load configuration specific to the local host
+(let ((local-config-file "~/.emacs.d/local-config"))
+  (if (file-exists-p (concat local-config-file ".el"))
+      (load local-config-file)))
