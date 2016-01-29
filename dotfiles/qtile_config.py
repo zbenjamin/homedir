eaf3112f4ddff3e9ed659884f8de0f455f063d49
Zev Benjamin <zev@dropbox.com>
2016-01-28 17:39:06 -0800

    First pass at a reasonable qtile config

--- dotfiles/qtile_config.py
+++ dotfiles/qtile_config.py
@@ -26,7 +26,27 @@
 
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
-from libqtile import layout, bar, widget
+from libqtile import layout, bar, hook, widget
+
+import logging
+import os
+import subprocess
+
+@hook.subscribe.startup_once
+def dbus_register():
+    x = os.environ['DESKTOP_AUTOSTART_ID']
+    subprocess.Popen(['dbus-send',
+                      '--session',
+                      '--print-reply',
+                      '--dest=org.gnome.SessionManager',
+                      '/org/gnome/SessionManager',
+                      'org.gnome.SessionManager.RegisterClient',
+                      'string:qtile',
+                      'string:' + x])
+
+@hook.subscribe.startup_once
+def setup_keyboard():
+    subprocess.Popen(['setxkbmap', '-layout', 'us', '-option', 'ctrl:nocaps'])
 
 mod = "mod4"
 
@@ -39,6 +59,16 @@ keys = [
     Key(
         [mod], "j",
         lazy.layout.up()
+    ),
+
+    # Move windows to next or prev stack
+    Key(
+        [mod, "control", "shift"], "k",
+        lazy.layout.client_to_prev()
+    ),
+    Key(
+        [mod, "control", "shift"], "j",
+        lazy.layout.client_to_next()
     ),
 
     # Move windows up or down in current stack
@@ -71,16 +101,75 @@ keys = [
         [mod, "shift"], "Return",
         lazy.layout.toggle_split()
     ),
-    Key([mod], "Return", lazy.spawn("xterm")),
+    Key([mod], "Return", lazy.spawn("gnome-terminal")),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout()),
     Key([mod], "w", lazy.window.kill()),
 
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
     Key([mod], "r", lazy.spawncmd()),
+    Key([mod], "x", lazy.qtilecmd()),
+
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "l", lazy.spawn('gnome-screensaver-command -l')),
+    Key([mod, "control", "shift"], "q", lazy.spawn('gnome-session-quit --logout --no-prompt')),
 ]
+
+def find_global_stack(screens, n):
+    """Returns the tuple of <screen idx, layout, local stack idx>"""
+    offset = 0
+    for i, screen in enumerate(screens):
+        group = screen.group
+        lay = group.layout
+        if isinstance(lay, layout.Stack):
+            if n - 1 < offset + len(lay.stacks):
+                return i, lay, n - offset - 1
+            offset += len(lay.stacks)
+        elif isinstance(lay, layout.Max):
+            if n - 1 == offset:
+                return i, lay, 0
+            offset += 1
+        else:
+            # Handle other layouts?
+            offset += 1
+
+    return -1, None, -1
+
+def switch_to_global_stack(n):
+    def callback(qtile):
+        screen_idx, lay, stack_idx = find_global_stack(qtile.screens, n)
+        if screen_idx == -1:
+            return
+
+        qtile.toScreen(screen_idx)
+        if isinstance(lay, layout.Stack):
+            lay.group.focus(lay.stacks[stack_idx].cw)
+        elif isinstance(lay, layout.Max):
+            lay.group.focus(lay.current)
+
+    return callback
+
+def send_to_global_stack(n):
+    def callback(qtile):
+        screen_idx, target_layout, stack_idx = find_global_stack(qtile.screens, n)
+        if screen_idx == -1:
+            return
+
+        cur_win = qtile.currentWindow
+        cur_layout = qtile.currentLayout
+        cur_group = qtile.currentGroup
+
+        cur_layout.remove(cur_win)
+        cur_group.remove(cur_win)
+        target_layout.group.add(cur_win)
+        if isinstance(target_layout, layout.Stack):
+            target_layout.cmd_client_to_stack(stack_idx)
+
+    return callback
+
+for i in range(1, 9):
+    keys.append(Key([mod], str(i), lazy.function(switch_to_global_stack(i))))
+    keys.append(Key([mod, "control"], str(i), lazy.function(send_to_global_stack(i))))
 
 groups = [Group(i) for i in "asdf"]
 
@@ -90,9 +179,9 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen())
     )
 
-    # mod1 + shift + letter of group = switch to & move focused window to group
+    # mod1 + control + letter of group = switch to & move focused window to group
     keys.append(
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name))
+        Key([mod, "control"], i.name, lazy.window.togroup(i.name))
     )
 
 layouts = [
@@ -102,25 +191,28 @@ layouts = [
 
 widget_defaults = dict(
     font='Ubuntu Medium',
-    fontsize=12,
+    fontsize=14,
     padding=3,
 )
 
-screens = [
-    Screen(
-        bottom=bar.Bar(
+def makebar():
+    return bar.Bar(
             [
                 widget.GroupBox(),
                 widget.Prompt(),
-                widget.WindowName(),
+                widget.TaskList(),
                 widget.Systray(),
                 widget.CPUGraph(),
                 widget.BatteryIcon(),
+#                widget.Volume(),
                 widget.Clock(format='%Y-%m-%d %a %H:%M'),
             ],
             30,
-        ),
-    ),
+        )
+
+screens = [
+    Screen(top=makebar()),
+    Screen(top=makebar())
 ]
 
 # Drag floating layouts.
@@ -134,7 +226,6 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []
-main = None
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
@@ -150,3 +241,24 @@ auto_fullscreen = True
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
+
+@hook.subscribe.client_new
+def new_client(client):
+    # Banish the Gnome "desktop" window
+    if client.window.get_wm_type() == 'desktop':
+        client.static(0)
+
+def setup_screens():
+    subprocess.Popen(['xrandr', '--output', 'VIRTUAL1', '--off', '--output',
+                      'eDP1', '--primary', '--auto', '--pos', '0x0', '--rotate',
+                      'normal', '--output', 'DP1', '--off', '--output', 'HDMI2',
+                      '--off', '--output', 'HDMI1', '--off', '--output', 'DP2',
+                      '--auto', '--pos', '1920x0'])
+
+@hook.subscribe.screen_change
+def restart_on_screen_change(qtile, ev):
+    setup_screens()
+    qtile.cmd_restart()
+
+def main(qtile):
+    pass
