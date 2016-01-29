3ad328bd58b2f10fea597d17386b7098c818f746
Zev Benjamin <zev@dropbox.com>
2016-01-28 17:47:13 -0800

    Reformat keybindings

--- dotfiles/qtile_config.py
+++ dotfiles/qtile_config.py
@@ -52,61 +52,34 @@ mod = "mod4"
 
 keys = [
     # Switch between windows in current stack pane
-    Key(
-        [mod], "k",
-        lazy.layout.down()
-    ),
-    Key(
-        [mod], "j",
-        lazy.layout.up()
-    ),
+    Key([mod], "k", lazy.layout.down()),
+    Key([mod], "j", lazy.layout.up()),
 
     # Move windows to next or prev stack
-    Key(
-        [mod, "control", "shift"], "k",
-        lazy.layout.client_to_prev()
-    ),
-    Key(
-        [mod, "control", "shift"], "j",
-        lazy.layout.client_to_next()
-    ),
+    Key([mod, "control", "shift"], "k", lazy.layout.client_to_prev()),
+    Key([mod, "control", "shift"], "j", lazy.layout.client_to_next()),
 
     # Move windows up or down in current stack
-    Key(
-        [mod, "control"], "k",
-        lazy.layout.shuffle_down()
-    ),
-    Key(
-        [mod, "control"], "j",
-        lazy.layout.shuffle_up()
-    ),
+    Key([mod, "control"], "k", lazy.layout.shuffle_down()),
+    Key([mod, "control"], "j", lazy.layout.shuffle_up()),
 
     # Switch window focus to other pane(s) of stack
-    Key(
-        [mod], "space",
-        lazy.layout.next()
-    ),
+    Key([mod], "space", lazy.layout.next()),
 
     # Swap panes of split stack
-    Key(
-        [mod, "shift"], "space",
-        lazy.layout.rotate()
-    ),
+    Key([mod, "shift"], "space", lazy.layout.rotate()),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key(
-        [mod, "shift"], "Return",
-        lazy.layout.toggle_split()
-    ),
-    Key([mod], "Return", lazy.spawn("gnome-terminal")),
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout()),
-    Key([mod], "w", lazy.window.kill()),
 
+    Key([mod], "Return", lazy.spawn("gnome-terminal")),
+    Key([mod], "w", lazy.window.kill()),
     Key([mod], "r", lazy.spawncmd()),
     Key([mod], "x", lazy.qtilecmd()),
 
